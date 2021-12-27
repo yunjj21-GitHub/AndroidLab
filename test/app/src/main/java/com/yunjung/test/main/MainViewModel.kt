eea978934052f69c89d9a8066d3cdc96d2e95b13
yunjung <yunjj21@hs.ac.kr>
2021-12-28 01:15:19 +0900

    MVVM 패턴 적용
    MVVM 패턴 적용

--- test/app/src/main/java/com/yunjung/test/main/MainViewModel.kt
+++ test/app/src/main/java/com/yunjung/test/main/MainViewModel.kt
@@ -1,6 +1,19 @@
 package com.yunjung.test.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
+    // 클래스 내부에서 접근할 데이터 (수정 가능)
+    private val _value = MutableLiveData<Int>(0)
+
+    // 클래스 외부에서 접근할 데이터 (수정 불가능)
+    val value : LiveData<Int>
+        get() = _value // 클래스 외부에서 접근할때엔 해당 변수로 접근해 내부에서 사용하는 변수를 get으로 반환
+
+    // 데이터를 갱신할 때 사용할 메소드
+    fun updateValue(){
+
+    }
 }
