8620694b4ac9ee79dabd074e75c797d1ccf29717
yunjung <yunjj21@hs.ac.kr>
2021-12-31 20:06:18 +0900

    네이버 지도 API 적용
    네이버 지도 API 적용

--- test/app/src/main/java/com/yunjung/practice/one/OneFragment.kt
+++ test/app/src/main/java/com/yunjung/practice/one/OneFragment.kt
@@ -6,8 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.naver.maps.map.MapFragment
 import com.yunjung.practice.R
 import com.yunjung.practice.databinding.FragmentOneBinding
 
@@ -36,5 +38,16 @@ class OneFragment : Fragment() {
         moveBtn.setOnClickListener {
             findNavController().navigate(R.id.action_oneFragment_to_twoFragment)
         }
+
+        // 네이버 지도를 화면에 표시
+        attachNaverMap(childFragmentManager)
     }
+}
+
+// 네이버 지도를 화면에 표시하는 함수
+fun attachNaverMap(fm : FragmentManager){
+    val mapFragment = fm.findFragmentById(R.id.map) as MapFragment?
+        ?: MapFragment.newInstance().also {
+            fm.beginTransaction().add(R.id.map, it).commit()
+        }
 }
