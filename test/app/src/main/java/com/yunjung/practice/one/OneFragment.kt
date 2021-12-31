0c4e68aabf0b06aac38d9885e2f04870ed8cfd2a
yunjung <yunjj21@hs.ac.kr>
2022-01-01 00:59:20 +0900

    네이버 지도에 현재 위치 추가
    네이버 지도에 현재 위치 추가

--- test/app/src/main/java/com/yunjung/practice/one/OneFragment.kt
+++ test/app/src/main/java/com/yunjung/practice/one/OneFragment.kt
@@ -1,5 +1,6 @@
 package com.yunjung.practice.one
 
+import android.Manifest
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,13 +10,29 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.naver.maps.map.LocationTrackingMode
 import com.naver.maps.map.MapFragment
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.util.FusedLocationSource
 import com.yunjung.practice.R
 import com.yunjung.practice.databinding.FragmentOneBinding
 
-class OneFragment : Fragment() {
+class OneFragment : Fragment(), OnMapReadyCallback {
     lateinit var binding : FragmentOneBinding
     lateinit var viewModel : OneViewModel
+
+    // 네이버 지도 API 관련
+    private lateinit var locationSource: FusedLocationSource
+    private lateinit var naverMap: NaverMap
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
+        private val PERMISSIONS : Array<String> = arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        )
+    }
 
     // 뷰가 생성될 때 실행
     override fun onCreateView(
@@ -39,15 +56,43 @@ class OneFragment : Fragment() {
             findNavController().navigate(R.id.action_oneFragment_to_twoFragment)
         }
 
-        // 네이버 지도를 화면에 표시
-        attachNaverMap(childFragmentManager)
-    }
-}
+        /* 네이버 지도 API 관련 */
+        val mapFragment = displayMapOnScreen(childFragmentManager) // 네이버 지도를 화면에 표시
+        mapFragment.getMapAsync(this) // 인터페이스 역할을 하는 naverMap 객체를 얻어옴
 
-// 네이버 지도를 화면에 표시하는 함수
-fun attachNaverMap(fm : FragmentManager){
-    val mapFragment = fm.findFragmentById(R.id.map) as MapFragment?
-        ?: MapFragment.newInstance().also {
-            fm.beginTransaction().add(R.id.map, it).commit()
+        // 지도에 현재 위치 추가
+        locationSource =
+            FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
+    }
+
+    // 네이버 지도 뷰를 생성
+    fun displayMapOnScreen(fm : FragmentManager) : MapFragment{
+        return fm.findFragmentById(R.id.map) as MapFragment?
+            ?: MapFragment.newInstance().also {
+                fm.beginTransaction().add(R.id.map, it).commit()
+            }
+    }
+
+    // naverMap 객체가 준비되면 실행 됨
+    override fun onMapReady(naverMap: NaverMap) {
+        this.naverMap = naverMap
+        naverMap.locationSource = locationSource
+        requestPermissions(PERMISSIONS, LOCATION_PERMISSION_REQUEST_CODE) // 사용자에게 권한을 요청함
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>,
+                                            grantResults: IntArray) {
+        if (locationSource.onRequestPermissionsResult(requestCode, permissions,
+                grantResults)) {
+            if (!locationSource.isActivated) { // 권한이 거부 되었다면
+                naverMap.locationTrackingMode = LocationTrackingMode.None
+                return
+            }
+            else{ // 권한이 인가되었다면
+                naverMap.locationTrackingMode = LocationTrackingMode.Follow
+            }
         }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
 }
