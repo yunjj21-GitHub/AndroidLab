1dd4764dabc0b8b3355f95db23c4bc1791e9c8d9
yunjung <yunjj21@hs.ac.kr>
2022-01-03 18:30:02 +0900

    MainActivity 수정
    MainActivity 수정

--- test/app/src/main/java/com/yunjung/practice/main/MainActivity.kt
+++ test/app/src/main/java/com/yunjung/practice/main/MainActivity.kt
@@ -26,13 +26,9 @@ class MainActivity : AppCompatActivity() {
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         binding.viewModel = viewModel
 
-        // 필요한 뷰를 가져옴
-        val toolbar = binding.toolbar
-        val titleView = binding.titleView
-
         /* Navigation Component 관련 */
         // 툴바 설정
-        setSupportActionBar(toolbar)
+        setSupportActionBar(binding.toolbar)
         supportActionBar?.setDisplayShowTitleEnabled(false) // 안드로이드에서 자동으로 넣는 툴바의 타이틀 제거
 
         // NavHost를 이용하여 NavController가져오기
@@ -45,20 +41,7 @@ class MainActivity : AppCompatActivity() {
         // 액션바에 navController와 appBarConfiguration객체를 설정
         setupActionBar(navController, appBarConfiguration)
 
-        // 탐색이 수행 될 마다 실행됨
-        navController.addOnDestinationChangedListener { _, destination, _ ->
-            // back button icon 이미지 설정 or Toolbar title 설정 etc..
-            // ex)
-            when (destination.id) {
-                R.id.oneFragment -> {
-                    titleView.text = "oneFragment"
-                }
-                R.id.twoFragment -> {
-                    toolbar.setNavigationIcon(R.drawable.ic_bacck)
-                    titleView.text = "twoFragment"
-                }
-            }
-        }
+        setTitleAndBackButton(navController)
     }
 
     // 툴바를 액션바로 지정
@@ -71,5 +54,32 @@ class MainActivity : AppCompatActivity() {
     // back button이 동작하도록 함
     override fun onSupportNavigateUp(): Boolean {
         return host.navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    // 전환되는 Fragment에 따라 적절한 Title과 BackButton이 보이도록 함
+    private fun setTitleAndBackButton(navController : NavController){
+        val toolbar = binding.toolbar
+        val title = binding.titleView
+
+        // 탐색이 수행 될 마다 실행됨
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            // back button icon 이미지 설정 or Toolbar title 설정 etc..
+            // ex)
+            toolbar.setNavigationIcon(R.drawable.ic_bacck)
+            when (destination.id) {
+                R.id.oneFragment -> {
+                    title.text = "oneFragment"
+                }
+                R.id.twoFragment -> {
+                    title.text = "twoFragment"
+                }
+                R.id.threeFragment ->{
+                    title.text = "threeFragment"
+                }
+                R.id.fourFragment -> {
+                    title.text = "fourFragment"
+                }
+            }
+        }
     }
 }
