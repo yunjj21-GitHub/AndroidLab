bf841589bdb0dcd6260f50159963455089084f04
yunjung <yunjj21@hs.ac.kr>
2022-01-11 15:27:46 +0900

    Glide 라이브러리 사용하여 이미지 로드하고 뷰에 표시하기
    Glide 라이브러리 사용하여 이미지 로드하고 뷰에 표시하기

--- test/app/src/main/java/com/yunjung/practice/news/NewsFragment.kt
+++ test/app/src/main/java/com/yunjung/practice/news/NewsFragment.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
 import com.yunjung.practice.R
 import com.yunjung.practice.databinding.FragmentNewsBinding
 
@@ -30,5 +32,17 @@ class NewsFragment : Fragment() {
 
         viewModel = ViewModelProvider(this).get(NewsViewModel::class.java)
         binding.viewModel = viewModel
+
+        // imageView 이미지 처리
+        imageLoadAndDisplay(binding.imageView, "https://w.namu.la/s/43d991eb90905f3532cac04e8764f211911dccc3f80f952dd2f2e57579b15a48f2e89403659b830a0fbf1b0ef21898da36da076dd20ab6132e46c4ec7d5fd5a2da55eb6c0835a4113fea76e8b354bc660540218b8160da4e9d314538163f6f0e93d58b19a43588a591f738be9b0fe2d9")
+    }
+
+    // 서버에서 이미지를 로드하여 뷰에 표시하는 메서드
+    private fun imageLoadAndDisplay(imageView : ImageView, imageUrl : String){
+        Glide.with(this)
+            .load(imageUrl)
+            .placeholder(R.drawable.loading)
+            .error(R.drawable.brocked_image)
+            .into(imageView)
     }
 }
